f2f44f812c2d468ceebaf77bfa501fa70e373fe6
“elisanuness” <elisanunessf.04@gmail.com>
2025-11-02 11:49:40 -0300

    feat: Inserindo e configurando fontes e paleta de cores

--- Frontend Angular/tailwind.config.js
+++ Frontend Angular/tailwind.config.js
@@ -1,10 +1,38 @@
+/*
+# =====================================================================================
+# Projeto RR-Nexus
+# Versão: 1.0.0
+# Autor(es): Elisa / Equipe
+# Data: 02/11/2025
+# Descrição: Definição de estilo global para o frontend.
+
+# Alteração: Inserção da fonte e paleta de cores.
+# =====================================================================================
+*/
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'baseet': ['29LT Baseet', 'sans-serif'],
+      },
+      colors: {
+        'nexus-darkest': '#130220',
+        'nexus-dark': '#220A35',
+        'nexus-purple-deep': '#5B0772',
+        'nexus-purple': '#8C0590',
+        'nexus-magenta': '#EA1EF3',
+        'nexus-lavender': '#B17BC5',
+        'nexus-pink-light': '#EBA4E6',
+        'nexus-pink-pale': '#FCE8ED',
+        'nexus-cream': '#FFFEE3',
+      },
+    },
   },
   plugins: [],
 }
