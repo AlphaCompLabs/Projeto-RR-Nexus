2ece853c9ad6a4729468f5e1c94fc4f3eea0fa40
“elisanuness” <elisanunessf.04@gmail.com>
2025-11-03 14:12:32 -0300

    fix: Botão LOGIN Header

--- Frontend Angular/src/app/app.config.ts
+++ Frontend Angular/src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// ARQUIVO: src/app/app.config.ts
 
+import { ApplicationConfig } from '@angular/core';
+
+// 1. Importe a função 'withInMemoryScrolling'
+import { 
+  provideRouter, 
+  withInMemoryScrolling 
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    
+    // 2. Adicione a feature 'withInMemoryScrolling'
+    provideRouter(
+      routes,
+      
+      // 3. Passe as opções de scroll para DENTRO dela
+      withInMemoryScrolling({
+        // Habilita o scroll para #id (ancoras)
+        anchorScrolling: 'enabled',
+        
+        // Habilita o scroll para o topo ao recarregar/navegar
+        scrollPositionRestoration: 'top'
+      })
+    )
   ]
 };
--- Frontend Angular/src/app/components/header/header.component.ts
+++ Frontend Angular/src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+// 1. IMPORTE O ViewportScroller
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [], 
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor(private scroller: ViewportScroller) {}
+
+  public scrollToLogin(): void {
+    this.scroller.scrollToAnchor('login-form-section');
+  }
 }
