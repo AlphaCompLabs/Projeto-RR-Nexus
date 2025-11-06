6de33c4b1244d58cff03b506735899dff90e586b
“elisanuness” <elisanunessf.04@gmail.com>
2025-11-06 00:00:16 -0300

    fix: URL2 da API

--- Frontend Angular/src/app/services/auth.service.ts
+++ Frontend Angular/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AuthService {
 
-  private readonly API_URL = 'http://www.meutrabalho.com.br/api/auth';
+  private readonly API_URL = 'http://172.19.50.25:3000/api/auth';
   private readonly SESSION_KEY = 'rr-nexus-session-id';
 
   // --- NOSSOS "ESTADOS" GLOBAIS ---
