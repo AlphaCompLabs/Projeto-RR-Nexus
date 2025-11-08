788c72aed94971ab5f29b6dd57cb698dbe10b8ee
Diogo <diogof040@gmail.com>
2025-11-08 17:49:07 -0300

    fix: porta 3000 removida auth.service

--- Frontend Angular/src/app/services/auth.service.ts
+++ Frontend Angular/src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AuthService {
 
   // O 'API_URL' agora aponta para o seu Servidor de Backend (D)
-  private readonly API_URL = 'http://172.19.50.25:3000/api/auth'; 
+  private readonly API_URL = 'http://172.19.50.25/api/auth'; 
   // O 'HOST_URL' aponta para os seus Servidores HTTP (A, B, C)
   private readonly HOST_URL = 'http://www.meutrabalho.com.br';
   private readonly SESSION_KEY = 'rr-nexus-session-id';
